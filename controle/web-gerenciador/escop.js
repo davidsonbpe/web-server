fad2e9794f6027935b9c57c4ecb9597934c89f28
Davidson Bpe <davidsomixer@gmail.com>
2016-09-08 21:06:11 -0300

    Add files via upload

---
+++ controle/web-gerenciador/escop.js
@@ -0,0 +1,23 @@
+
+// DEVELOPER DAVIDSONBPE © 2016 www.davidsonbpe.blogspot.com , Inc. //
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49856760-1', 'auto');
+  ga('send', 'pageview');
+
+
+var mensagem="";
+function clickIE() {if (document.all) {(mensagem);return false;}}
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(mensagem);return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false")
+// -->
